41637169bc4f0ca308d97357311520a6fba04824
BrandonBellick <145628105+BrandonBellick@users.noreply.github.com>
2023-10-05 14:51:47 -0700

    bfg

---
+++ AboutMeJS.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const pageWrapper1 = document.querySelector('.page-wrapper1');
+    const pageWrapper2 = document.querySelector('.page-wrapper2');
+    const pageWrapper3 = document.querySelector('.page-wrapper3');
+  
+    const button1 = document.querySelector('.option1');
+    const button2 = document.querySelector('.option2');
+    const button3 = document.querySelector('.option3');
+  
+    button1.addEventListener('click', () => {
+      if (pageWrapper1.style.display === 'none' || pageWrapper1.style.display === '') {
+        pageWrapper1.style.display = 'flex';
+        
+        pageWrapper2.style.display = 'none';
+        pageWrapper3.style.display = 'none';
+      } else {
+        pageWrapper1.style.display = 'none';
+      }
+    });
+  
+    button2.addEventListener('click', () => {
+      if (pageWrapper2.style.display === 'none' || pageWrapper2.style.display === '') {
+        pageWrapper2.style.display = 'flex';
+        pageWrapper1.style.display = 'none';
+        pageWrapper3.style.display = 'none';
+      } else {
+        pageWrapper2.style.display = 'none';
+      }
+    });
+  
+    button3.addEventListener('click', () => {
+      if (pageWrapper3.style.display === 'none' || pageWrapper3.style.display === '') {
+        pageWrapper3.style.display = 'flex';
+        pageWrapper1.style.display = 'none';
+        pageWrapper2.style.display = 'none';
+      } else {
+        pageWrapper3.style.display = 'none';
+      }
+    });
+  });
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('.options-wrapper button');
+  
+    buttons.forEach(button => {
+      button.addEventListener('click', () => {
+        buttons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+      });
+    });
+  });
+  
