e626b501ef4d4bf5f7ce66f92e15ac4e0cad6f15
BrandonBellick <145628105+BrandonBellick@users.noreply.github.com>
2023-10-07 12:11:38 -0700

    lmup

--- innovation.js
+++ innovation.js
@@ -172,3 +172,31 @@ patent3Button.addEventListener("click", () => {
     });
   });
   
+  document.addEventListener("DOMContentLoaded", function () {
+    // Find the button element by its class
+    var btnToggle = document.querySelector(".btn-toggle");
+    
+    // Find the light and dark mode stylesheets by their IDs
+    var lightModeCSS = document.getElementById("innovation-light");
+    var darkModeCSS = document.getElementById("innovation-dark");
+
+    // Initialize a counter
+    var clickCount = 0;
+
+    // Add a click event listener to the button
+    btnToggle.addEventListener("click", () => {
+        if (lightModeCSS.disabled) {
+            // Enable light mode
+            lightModeCSS.removeAttribute('disabled');
+            darkModeCSS.setAttribute('disabled', 'true');
+        } else {
+            // Enable dark mode
+            darkModeCSS.removeAttribute('disabled');
+            lightModeCSS.setAttribute('disabled', 'true');
+        }
+
+        // Increment the counter and print it to the console
+        clickCount++;
+        console.log("Button clicked " + clickCount + " times");
+    });
+});
